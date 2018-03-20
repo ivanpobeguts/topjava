40f73a87c949784b7ed8c05a153ed6047cebac84
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-03-20 17:04:48 +0300

    fix all comments for HW3

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -8,18 +8,12 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.CollectionUtils;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
-import ru.javawebinar.topjava.util.DateTimeUtil;
 
 import javax.sql.DataSource;
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 @Repository
 public class JdbcMealRepositoryImpl implements MealRepository {
@@ -44,7 +38,6 @@ public class JdbcMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
-//        userRepository.get(userId);
         MapSqlParameterSource map = new MapSqlParameterSource()
                 .addValue("id", meal.getId())
                 .addValue("datetime", meal.getDateTime())
@@ -82,15 +75,7 @@ public class JdbcMealRepositoryImpl implements MealRepository {
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return getAllFiltered(userId, meal -> DateTimeUtil.isBetween(meal.getDateTime(), startDateTime, endDateTime));
-    }
-
-    private List<Meal> getAllFiltered(int userId, Predicate<Meal> filter) {
-        List<Meal> meals = getAll(userId);
-        return CollectionUtils.isEmpty(meals) ? Collections.emptyList() :
-                meals.stream()
-                        .filter(filter)
-                        .sorted(Comparator.comparing(Meal::getDateTime).reversed())
-                        .collect(Collectors.toList());
+        return jdbcTemplate.query
+                ("SELECT * FROM meals WHERE user_id=? AND datetime >= ? AND datetime <= ? ORDER BY datetime DESC", ROW_MAPPER, userId, startDateTime, endDateTime);
     }
 }
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -10,16 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
-import java.lang.reflect.Array;
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
 
-import static org.junit.Assert.*;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
@@ -32,11 +25,11 @@ import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 public class MealServiceTest {
 
     @Autowired
-    MealService service;
+    private MealService service;
 
     @Test
     public void get() throws Exception {
-        assertThat(meal1.toString(), is(service.get(MEAL_ID, START_SEQ).toString()));
+        assertMatch(service.get(MEAL_ID, START_SEQ), meal1);
     }
 
     @Test(expected = NotFoundException.class)
@@ -47,7 +40,7 @@ public class MealServiceTest {
     @Test
     public void delete() throws Exception{
         service.delete(MEAL_ID, START_SEQ);
-        assertThat(Arrays.asList(meal6, meal5, meal4, meal3, meal2).toString(), is(service.getAll(START_SEQ).toString()));
+        assertMatch(service.getAll(START_SEQ), meal6, meal5, meal4, meal3, meal2);
     }
 
     @Test(expected = NotFoundException.class)
@@ -57,21 +50,20 @@ public class MealServiceTest {
 
     @Test
     public void getBetweenDateTimes() {
-        assertThat(Arrays.asList(meal2, meal1).toString(),
-                is(service.getBetweenDateTimes(LocalDateTime.of(2015, Month.MAY, 30, 8, 0),
-                        LocalDateTime.of(2015, Month.MAY, 30, 14, 0), START_SEQ).toString()));
+        assertMatch(service.getBetweenDateTimes(LocalDateTime.of(2015, Month.MAY, 30, 8, 0),
+                LocalDateTime.of(2015, Month.MAY, 30, 14, 0), START_SEQ), meal2, meal1);
     }
 
     @Test
     public void getAll() throws Exception {
-        assertThat(Arrays.asList(meal6, meal5, meal4, meal3, meal2, meal1).toString(), is(service.getAll(START_SEQ).toString()));
+        assertMatch(service.getAll(START_SEQ), meal6, meal5, meal4, meal3, meal2, meal1);
     }
 
     @Test
     public void update() throws Exception {
         Meal updated = new Meal(MEAL_ID, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 600);
         service.update(updated, START_SEQ);
-        assertThat(updated.toString(), is(service.get(MEAL_ID, START_SEQ).toString()));
+        assertMatch(service.get(MEAL_ID, START_SEQ), updated);
     }
 
     @Test(expected = NotFoundException.class)
@@ -84,6 +76,6 @@ public class MealServiceTest {
     public void create() {
         Meal newMeal = new Meal(null, LocalDateTime.of(2015, Month.MAY, 29, 10, 0), "Завтрак", 600);
         service.create(newMeal, START_SEQ);
-        assertThat(Arrays.asList(meal6, meal5, meal4, meal3, meal2, meal1, newMeal).toString(), is(service.getAll(START_SEQ).toString()));
+        assertMatch(service.getAll(START_SEQ), meal6, meal5, meal4, meal3, meal2, meal1, newMeal);
     }
 }
