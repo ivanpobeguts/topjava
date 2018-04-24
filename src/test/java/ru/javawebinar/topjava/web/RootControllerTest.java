4b8de48b9f40ba0879aad22586c0d33b231e3e15
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-04-24 12:24:23 +0300

    Revert

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.javawebinar.topjava.UserTestData.USER;
-import static ru.javawebinar.topjava.MealTestData.MEAL1;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class RootControllerTest extends AbstractControllerTest {
@@ -28,20 +27,4 @@ public class RootControllerTest extends AbstractControllerTest {
                 )));
     }
 
-    @Test
-    public void testMeals() throws Exception {
-        mockMvc.perform(get("/meals"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("meals"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
-                .andExpect(model().attribute("meals", hasItem(
-                        allOf(
-                                hasProperty("id", is(START_SEQ + 2)),
-                                hasProperty("calories", is(MEAL1.getCalories())),
-                                hasProperty("description", is(MEAL1.getDescription())),
-                                hasProperty("dateTime", is(MEAL1.getDateTime()))
-                        )
-                )));
-    }
 }
