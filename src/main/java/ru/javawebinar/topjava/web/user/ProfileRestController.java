23c6c9b5494472e29b76488ab6b79507f1d3cdde
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-04-24 12:26:19 +0300

    Revert "7 12 soapui utf8 converter"
    This reverts commit efe37bc90e679e6be48728a4043478e2680ff81b.

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
@@ -1,34 +1,21 @@
 package ru.javawebinar.topjava.web.user;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.User;
 
-@RestController
-@RequestMapping(ProfileRestController.REST_URL)
+@Controller
 public class ProfileRestController extends AbstractUserController {
-    static final String REST_URL = "/rest/profile";
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User get() {
         return super.get(AuthorizedUser.id());
     }
 
-    @DeleteMapping
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete() {
         super.delete(AuthorizedUser.id());
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void update(@RequestBody User user) {
+    public void update(User user) {
         super.update(user, AuthorizedUser.id());
-    }
-
-    @GetMapping(value = "/text")
-    public String testUTF() {
-        return "Русский текст";
     }
 }
